d52c7697c821da86bbe9be15583e2dd8c11c2340
Per Christian B. Viken <perchr@northblue.org>
2012-10-10 21:23:45 +0200

    Quick view and test

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module TurboTrouble
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.precompile += ['print.css']
   end
 end
