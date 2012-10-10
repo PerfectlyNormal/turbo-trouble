630d2c6f6bde813b235a02523beea0c24a324a0c
Per Christian B. Viken <perchr@northblue.org>
2012-10-10 21:05:09 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TurboTrouble::Application.config.secret_token = '76257a4b289eb54a6423457cdfeab9d39b99e18206a0d7544ab24b2ac3f04b47e33492537fc1af4e76d6161229dcdad4194df342a6f93089e2e89e112b8733ff'
